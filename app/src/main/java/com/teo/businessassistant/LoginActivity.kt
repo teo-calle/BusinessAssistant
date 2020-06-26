253442972991c4de4ead12fc015f058b3dbb8b1d
teo <teodoro.calle@udea.edu.co>
2020-06-26 14:29:31 -0500

    finish

--- app/src/main/java/com/teo/businessassistant/LoginActivity.kt
+++ app/src/main/java/com/teo/businessassistant/LoginActivity.kt
@@ -1,11 +1,95 @@
 package com.teo.businessassistant
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    var correo: String? = ""
+    var contra: String? = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        var datosRecibidos : Bundle? =intent.extras
+        if(datosRecibidos != null){
+            correo = datosRecibidos?.getString("correo")
+            contra = datosRecibidos?.getString("contra")
+            //Toast.makeText(this,"return main to login",Toast.LENGTH_SHORT).show()
+        }
+
+
+        tv_registrar.setOnClickListener {
+
+            var intent =Intent(this,RegistroActivity::class.java)
+            startActivityForResult(intent,1001)
+
+
+
+        }
+
+        bt_iniciarsesion.setOnClickListener{
+
+            if (correo!!.isEmpty()||contra!!.isEmpty())
+                Toast.makeText(this,"Sus datos no coinciden",Toast.LENGTH_SHORT).show()
+
+            else{
+                if(et_Correo.text.toString()==correo && et_Contra.text.toString()==contra){
+                    Toast.makeText(this,"Valido ",Toast.LENGTH_SHORT).show()
+                    goToMainActivity()
+
+                }
+
+                else
+                    Toast.makeText(this,"Sus datos no coinciden ",Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
+
+
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+
+        if(requestCode==1001 && resultCode==Activity.RESULT_OK){
+            var datosRecibidos : Bundle? =data!!.extras
+            if(datosRecibidos != null){
+                correo = datosRecibidos?.getString("correo")
+                contra = datosRecibidos?.getString("contra")
+                //Toast.makeText(this,correo,Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
+    }
+
+
+    private fun goToMainActivity(){
+        var intent =Intent(this,MainActivity::class.java)
+        intent.putExtra("correo",correo)
+        intent.putExtra("contra",contra)
+
+
+        startActivity(intent)
+        finish()
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/teo/businessassistant/MainActivity.kt
+++ app/src/main/java/com/teo/businessassistant/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.teo.businessassistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var correo:String?=""
+    var contra:String?=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var datosRecibidos : Bundle? =intent.extras
+        if(datosRecibidos != null){
+            correo = datosRecibidos?.getString("correo")
+            contra = datosRecibidos?.getString("contra")
+
+            tv_correo.text=correo
+
+        }
+
+
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item1->{
+                Toast.makeText(this,"Sesion cerrada", Toast.LENGTH_SHORT).show()
+                goToLoginActivity( )
+
+
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun goToLoginActivity( ){
+        val intent = Intent(this,LoginActivity::class.java)
+        intent.putExtra("correo",correo)
+        intent.putExtra("contra",contra)
+        startActivity(intent)
+        finish()
+    }
+
+
 }
