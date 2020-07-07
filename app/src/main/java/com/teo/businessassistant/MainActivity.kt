b69057740dfa5f83c1eecadf6ef63488d2fabde4
Erica Figueroa <johana.figueroa@udea.edu.co>
2020-07-07 03:22:24 -0500

    actualizacion

--- app/src/main/java/com/teo/businessassistant/MainActivity.kt
+++ app/src/main/java/com/teo/businessassistant/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.teo.businessassistant
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         var datosRecibidos : Bundle? =intent.extras
         if(datosRecibidos != null){
@@ -55,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("contra",contra)
         startActivity(intent)
         finish()
+
+
     }
-
-
 }
